c6674bd0e3e3a3be28000826c63b1d8f3cde03a1
Jatin Batra <jatin.batra01@nagarro.com>
2024-04-09 01:10:53 +0530

    first commit

---
+++ src/webparts/spfxGithubDeployedApp/components/ISpfxGithubDeployedAppProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxGithubDeployedAppProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfxGithubDeployedApp/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxGithubDeployedAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxGithubDeployedAppWebPartStrings' {
+  const strings: ISpfxGithubDeployedAppWebPartStrings;
+  export = strings;
+}
